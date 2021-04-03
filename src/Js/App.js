1fd952e5140f6f93999530dfaa24db4b8c65a507
Utkarsh <agarwalutkarsh222@gmail.com>
2021-04-03 19:58:50 +0530

    Js fies and starter funcationality added

--- src/Js/App.js
+++ src/Js/App.js
@@ -1,1 +1,9 @@
-console.log("test");
+import { check } from "./Model/login_check";
+import login_template from "../views/loginPage.html";
+
+function login_load() {
+    document.getElementById("center_left").innerHTML = login_template;
+    document.getElementById("btn").addEventListener("click", check);
+}
+
+document.onload = login_load();
---
+++ src/Js/Model/login_check.js
@@ -0,0 +1,4 @@
+export const check = () => {
+    // console.log("checked");
+    
+}
