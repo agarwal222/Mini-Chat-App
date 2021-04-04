f54bda698e6d30f96d8b1ab74a629022c39daff7
Utkarsh <agarwalutkarsh222@gmail.com>
2021-04-04 17:47:48 +0530

    added join or create , UI , file tree changes

--- src/Js/Model/login_check.js
+++ src/Js/Model/login_check.js
@@ -1,4 +1,6 @@
+import create_or_join from "../../views/create_or_join.html";
+
 export const check = () => {
     // console.log("checked");
-    
+    document.getElementById("center_left").innerHTML = create_or_join;
 }
